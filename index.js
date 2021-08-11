868598922b2b96a2bdc098d83e9de164da594703
minlaxz <minminlaxz@gmail.com>
2021-08-12 02:52:52 +0630

    wrangler secret

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const resp = await fetch("https://api.unsplash.com/photos/", {
+    headers: {
+      'Authorization': `Client-ID ${unsplashKey}`
+    }
+  })
+  const data = await resp.json()
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-type': 'application/json'
+    },
+  })
+
+
   const body = await request.json()
   return new Response(JSON.stringify(body), {
     headers: {
