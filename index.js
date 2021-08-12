3dabecf48bafa14667a47a717d5b426bc82bdf72
minlaxz <minminlaxz@gmail.com>
2021-08-12 16:30:46 +0630

    Making query to unsplash

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const resp = await fetch("https://api.unsplash.com/photos/", {
+  // await is important
+  const { query } = await request.json()
+  console.log(query)
+  const resp = await fetch(`https://api.unsplash.com/search/photos?query=${query}`, {
     headers: {
       'Authorization': `Client-ID ${unsplashKey}`
     }
   })
   const data = await resp.json()
-  return new Response(JSON.stringify(data), {
-    headers: {
-      'Content-type': 'application/json'
-    },
-  })
-
-
-  const body = await request.json()
-  return new Response(JSON.stringify(body), {
+  const images = data.results.map(image => ({
+    id: image.id,
+    url: image.urls.small,
+    create_at: image.created_at,
+    updated_at: image.updated_at,
+    alt: image.alt_description,
+    // html: image.links.html, => https://unsplash.com/photos/$id
+  }))
+  return new Response(JSON.stringify(images), {
     headers: {
       'Content-type': 'application/json'
     },
